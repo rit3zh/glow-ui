6b9cdf9dffa49c60531399e07bbceba3b489c236
rit3zh <thisismyn07@gmail.com>
2025-05-10 19:29:31 -0400

    random

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
-// @github crisanne
-//
+// get
 import { CustomScrollView } from "@/components/templates/index";
 import { SpinnerRefreshIndicator } from "@/components/templates/scroll-view/resource/spinner/SpinnerRefreshIndicator";
 import { useRef, useState } from "react";
