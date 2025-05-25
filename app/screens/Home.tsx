682a68c8fd7abb09b56cde0be338f1f37a0a79cd
rit3zh <thisismyn07@gmail.com>
2025-05-25 18:57:44 +0530

    Update Home screen and remove unused files
    - Added CLI entry point in package.json for easier command line access.
    - Refactored Home screen to utilize AnimatedMaskedText for improved text display.
    - Removed songs.ts and artist-screen parallax components to clean up the codebase.
    - Updated typedoc.json to change output directory for documentation.
    - Enhanced Accordion component with animation effects for better user experience.

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,33 +1,32 @@
 import React, { useState } from "react";
-import { View, SafeAreaView, StyleSheet, Image } from "react-native";
+import { View, SafeAreaView, StyleSheet, Text } from "react-native";
 import { BACKGROUND_COLOR } from "../../app.config";
-import { ShimmerEffect } from "@/components/molecules/Shimmer/Shimmer";
+import {
+  Stepper,
+  StepperButton,
+  StepperContent,
+  StepperValue,
+  Accordion,
+  AccordionTrigger,
+  AccordionItem,
+  AccordionContent,
+  Center,
+  Row,
+  ExpandableButton,
+  AnimatedMaskedText,
+} from "@/components";
+import { SymbolView } from "expo-symbols";
 
 export const Home: React.FC = () => {
-  const [isTyping, setIsTyping] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
   return (
     <SafeAreaView style={styles.container}>
-      <ShimmerEffect
-        isLoading={isLoading}
-        shimmerAngle={25}
-        shimmerDuration={2000}
-        shimmerWidth={0.5}
-        blurTint="dark"
-        blurIntensity={100}
+      <AnimatedMaskedText
+        style={{
+          fontSize: 40,
+        }}
       >
-        <Image
-          onLoadEnd={() =>
-            setInterval(() => {
-              setIsLoading(false);
-            }, 4000)
-          }
-          source={{
-            uri: "https://www.pixelstalk.net/wp-content/uploads/images6/Aesthetic-Dark-Wallpaper-HD-Lonely.jpg",
-          }}
-          style={{ width: 500, height: 200 }}
-        />
-      </ShimmerEffect>
+        hey
+      </AnimatedMaskedText>
     </SafeAreaView>
   );
 };
@@ -35,9 +34,9 @@ export const Home: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: BACKGROUND_COLOR,
-    justifyContent: "center",
+    backgroundColor: "#000",
     alignItems: "center",
+    justifyContent: "center",
   },
   content: {
     flex: 1,
